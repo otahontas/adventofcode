ef3d548163b61a865c09e62b53cc92e86ca08965
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-10-21 17:00:32 +0300

    Add d16

---
+++ d16.py
@@ -0,0 +1,52 @@
+pattern = [1, 0, -1, 0]
+
+
+def tick1(a):
+    r = []
+    la = len(a)
+    for i in range(la):
+        res = 0
+        p_i = 0
+        reps = 0
+        j = i
+        while j < la:
+            res += a[j] * pattern[p_i]
+            reps += 1
+            if reps == i + 1:
+                reps = 0
+                p_i = 2 if p_i == 0 else 0
+                j += i + 1
+            j += 1
+        r.append(abs(res) % 10)
+    return r
+
+
+def tick2(a):
+    # faster loop through input
+    r = []
+    s = sum(a)
+    for i in a:
+        r.append(s % 10)
+        s -= i
+    return r
+
+
+s = open("inputs/d16.txt").read().strip()
+a = [int(x) for x in s]
+
+# first
+for i in range(100):
+    a = tick1(a)
+res = [str(x) for x in a[:8]]
+print("".join(res))
+
+
+# second
+offset = int(s[:7])
+a = [int(x) for x in s]
+b = a * 10000
+c = b[offset:]
+for i in range(100):
+    c = tick2(c)
+res = [str(x) for x in c[:8]]
+print("".join(res))
