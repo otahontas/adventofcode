10d8f50a9fd37de30a64504bbf909487b1306580
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-12-02 07:59:41 +0200

    Add d02 solution

--- 2020/d02.cpp
+++ 2020/d02.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-typedef long long ll;
-typedef vector<ll> vll;
-//typedef vector<ll> vll;
-//typedef pair<int, int> pi;
-//typedef vector<pi> vpi;
+typedef vector<int> vi;
+typedef vector<string> vs;
 
 #define REP(i,a,b) for (int i = a; i < b; i++)
-#define RREP(i,a,b) for (int i = a; i <= b; i++)
 #define F first
 #define S second
 #define PB push_back
 
-int n;
-vll v;
+int ans1, ans2;
 
-bool are_same(float a, float b) {
-    if (abs(a-b) < 1e-9) return true;
-    return false;
+vector<pair<vi,vs>> readInput() {
+    string s;
+    vector<pair<vi, vs>> inputs;
+    while (getline(cin, s) && s.length()) {
+        vi ints;
+        vs strings;
+        int a,b;
+        stringstream ss(s);
+        ss >> a >> b; ints.PB(a); ints.PB(-b);
+        string t,r;
+        size_t pos = s.find(":");
+        t = s.substr(pos-1, 1);
+        r = s.substr(pos+2);
+        strings.PB(t); strings.PB(r);
+        inputs.PB({ints, strings});
+    }
+    return inputs;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll x;
-    while (cin >> x) {
-        v.PB(x);
+
+    vector<pair<vi,vs>> v = readInput();
+    for (auto p : v) {
+        int a = p.F[0]; int b = p.F[1];
+        char c = p.S[0][0]; string r = p.S[1];
+        int counter = 0;
+        for (auto ch : r) {
+            if (ch == c) counter++;
+        }
+        if (counter >= a && counter <= b) ans1++;
+        if ((r[a-1] == c && r[b-1] != c) || (r[a-1] != c && r[b-1] == c)) ans2++;
     }
-    n = v.size();
-    cout << n << "\n";
-    REP(i,0,n) {
-        cout << v[i] << " ";
-    }
-    cout << "\n";
+    cout << ans1 << "\n" << ans2 << "\n";
 }
