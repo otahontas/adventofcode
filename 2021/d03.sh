5fb7da83fb3a6d9957d224515794bff925383c38
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-04 21:12:59 +0200

    d03 part 2

--- 2021/d03.sh
+++ 2021/d03.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "Part 01:"
 gamma=""
 
@@ -10,3 +11,41 @@ done <<< "$(awk '{$1=$1} 1' FS= inputs/d03.txt | rs -Tng0)"
 epsilon="$(echo $gamma | tr 01 10)"
 
 echo "$(( $((2#$gamma)) * $((2#$epsilon)) ))"
+
+echo "Part 02:"
+
+function count() {
+  mode=$1
+  local lines=$(<inputs/d03.txt)
+  local pos=1
+  local max_pos=13
+  while true; do
+    cols=$(echo "$lines" | awk '{$1=$1} 1' FS= | rs -Tng0 | sed -n "${pos}p")
+    read -r zeros ones <<< $(echo "$cols" | grep -o . | sort | uniq -c | awk '{print $1}')
+    if [ "$mode" == "oxygen" ]; then
+      popular=$([ $zeros -gt $ones ] && echo "0" || echo "1")
+    else
+      popular=$([ $zeros -le $ones ] && echo "0" || echo "1")
+    fi
+    rows_with_popular=$(echo "$cols" | sed 's/\(.\)/\1\n/g' | grep -n "$popular" | awk -F':' '{print $1}')
+    sed_cmd=-n
+    for row in $rows_with_popular; do
+      sed_cmd="$sed_cmd -e ${row}p"
+    done
+    lines=$(echo "$lines" | sed $sed_cmd)
+    amount=$(echo "$lines" | wc -l)
+    if [ "$amount" -eq "1" ]; then
+      break
+    fi
+    pos=$((pos + 1))
+    if [ $pos -gt $max_pos ]; then
+      pos=1
+    fi
+  done
+  echo $((2#$lines))
+}
+
+oxygen=$(count "oxygen")
+scrubber=$(count "scrubber")
+
+echo "$(( $oxygen * $scrubber ))"
