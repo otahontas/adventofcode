8b76786a4728c7bbf191cfbb6ed0a68e60c0b671
Otto Ahoniemi <otto@ottoahoniemi.fi>
2022-12-02 08:50:23 +0200

    fisk

--- 2022/01.ts
+++ 2022/01.ts
@@ -2,11 +2,11 @@ import { sum } from "lodash";
 import { readInput } from "./helpers";
 
 const descendingSums = readInput("01", "\n\n")
-	.reduce<number[]>(
-		(acc, curr) => [...acc, sum(curr.split("\n").map(Number))],
-		[],
-	)
-	.sort((a, b) => b - a);
+  .reduce<number[]>(
+    (acc, curr) => [...acc, sum(curr.split("\n").map(Number))],
+    [],
+  )
+  .sort((a, b) => b - a);
 
 console.log(`Part 1: ${descendingSums[0]}`);
 console.log(`Part 2: ${sum(descendingSums.slice(0, 3))}`);
--- 2022/helpers.ts
+++ 2022/helpers.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 export const readInput = (day: string, splitBy: string) =>
-	readFileSync(`inputs/${day}.txt`)
-		.toString()
-		.trimStart()
-		.trimEnd()
-		.split(splitBy);
+  readFileSync(`inputs/${day}.txt`)
+    .toString()
+    .trimStart()
+    .trimEnd()
+    .split(splitBy);
