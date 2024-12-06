56b0c2957b1b081c77691e4ce09efd3948393f8a
Otto Ahoniemi <otto@ottoahoniemi.fi>
2024-12-06 23:11:52 +0200

    Optimize 2024 d06

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -3,37 +3,28 @@ package main
 import (
 	"adventofcode/utils"
 	_ "embed"
-	"errors"
 	"fmt"
 )
 
 //go:embed input.txt
 var input string
 
-func getStartingPosition(grid []string) (complex128, error) {
+func getStartingPosition(grid []string) complex128 {
 	for y, line := range grid {
 		for x, char := range line {
 			if char == '^' {
-				return complex(float64(x), float64(y)), nil
+				return complex(float64(x), float64(y))
 			}
 		}
 	}
-	return complex(0, 0), errors.New("No starting position found")
+	return complex(0, 0)
 }
 
-func simulateRound(grid []string, new_obstacle complex128) (int, bool) {
+func simulateRoundAndReturnVisitedPoints(grid []string) []complex128 {
 	h, w := len(grid), len(grid[0])
 	visited := make(map[complex128]bool, 0)
-	position, err := getStartingPosition(grid)
-	if err != nil {
-		fmt.Println(err)
-		return -1, false
-	}
+	position := getStartingPosition(grid)
 	direction := complex(0, -1)
-	check_for_loops := new_obstacle != complex(-1, -1)
-	steps := 0
-	// greedy guess, if steps exceed this threshold, it's a loop. could be optimized
-	steps_threshold := (h * w) / 3
 	for {
 		visited[position] = true
 		new_position := position + direction
@@ -42,29 +33,54 @@ func simulateRound(grid []string, new_obstacle complex128) (int, bool) {
 		if x < 0 || x >= w || y < 0 || y >= h {
 			break
 		}
-		if check_for_loops && steps >= steps_threshold {
-			return -1, true
+		if grid[y][x] == '#' {
+			direction *= complex(0, 1) // turn right
+		} else {
+			position = new_position
+		}
+	}
+	visited_points := make([]complex128, len(visited))
+	i := 0
+	for k := range visited {
+		visited_points[i] = k
+		i++
+	}
+	return visited_points
+}
+
+func hasLoop(grid []string, new_obstacle complex128) bool {
+	h, w := len(grid), len(grid[0])
+	visited := make(map[complex128]int, 0)
+	position := getStartingPosition(grid)
+	direction := complex(0, -1)
+	for {
+		visited[position]++
+		new_position := position + direction
+		x := int(real(new_position))
+		y := int(imag(new_position))
+		if x < 0 || x >= w || y < 0 || y >= h {
+			break
+		}
+		// over 4 visits to the same cell means that we've
+		// arrived at least twice from same direction --> loop
+		if visited[position] > 4 {
+			return true
 		}
 		if grid[y][x] == '#' || new_position == new_obstacle {
 			direction *= complex(0, 1) // turn right
 		} else {
 			position = new_position
-			steps++
 		}
 	}
-	return len(visited), false
+	return false
 }
 
-func findLoops(grid []string) int {
+func findLoops(grid []string, visited_points []complex128) int {
 	loops := 0
-	for y, line := range grid {
-		for x, char := range line {
-			if char == '.' {
-				_, is_loop := simulateRound(grid, complex(float64(x), float64(y)))
-				if is_loop {
-					loops++
-				}
-			}
+	// faster if we only try to put new obstacles on visited points
+	for _, point := range visited_points {
+		if hasLoop(grid, point) {
+			loops++
 		}
 	}
 	return loops
@@ -72,8 +88,9 @@ func findLoops(grid []string) int {
 
 func main() {
 	grid := utils.SplitByDelimiterAndStrip(input)
-	ans1, _ := simulateRound(grid, complex(-1, -1))
-	ans2 := findLoops(grid)
+	visited_points := simulateRoundAndReturnVisitedPoints(grid)
+	ans1 := len(visited_points)
+	ans2 := findLoops(grid, visited_points)
 	fmt.Println(ans1)
 	fmt.Println(ans2)
 }
